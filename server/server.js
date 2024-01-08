0b9300ffd95cbfd9427667a3dbf7ab4cb3f6d67c
Aaloklowanshi <lowanshiaalok@gmail.com>
2024-01-08 20:59:47 +0530

    changed the directory structre

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ app.use("/api/v1/auth" , authRoutes);
 app.use("/api/v1/category" , categoryRoutes)
 app.use("/api/v1/product" , productRoutes);
 
-
+app.use(express.static(path.join(__dirname, 'client/build')));
 app.use('*' , function(req , res){
     res.sendFile(path.join(__dirname , './client/index.html'));
 })
